9539ec94c1d5dc6326903c7169494bda5d440aa5
Jan Kopal <joedmi189@gmail.com>
2021-04-12 19:54:34 +0200

    Fixed when user comes back from redirected site it wasn't redireting him anymore

--- Redirect/redirect.js
+++ Redirect/redirect.js
@@ -1,13 +1,17 @@
-var linksArray = [
-	"../index.html",
-	"https://www.youtube.com/watch?v=dQw4w9WgXcQ&pbjreload=101",
-	"../Secret/secret.html",
-];
-var randomNumber = Math.floor(Math.random() * linksArray.length);
-var link = linksArray[randomNumber];
+function redirect() {
+	var linksArray = [
+		"../index.html",
+		"https://www.youtube.com/watch?v=dQw4w9WgXcQ&pbjreload=101",
+		"../Secret/secret.html",
+	];
+	var randomNumber = Math.floor(Math.random() * linksArray.length);
+	var link = linksArray[randomNumber];
 
-console.log(`Changed link in text to ${link}`);
-document.getElementById("link").setAttribute("href", link);
+	console.log(`Changed link in text to ${link}`);
+	document.getElementById("link").setAttribute("href", link);
 
-console.log(`Redirecting to ${link}`);
-setTimeout(() => (window.location.href = link), 1000);
+	console.log(`Redirecting to ${link}`);
+	setTimeout(() => (window.location.href = link), 1000);
+}
+
+document.onload = redirect();
