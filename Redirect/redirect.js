d87a639fc85fed781bf0a0b40d8e77598cd78f04
Jan Kopal <joedmi189@gmail.com>
2021-04-12 19:31:52 +0200

    Written code to redirect to random page from array

---
+++ Redirect/redirect.js
@@ -0,0 +1,13 @@
+var linksArray = [
+	"../index.html",
+	"https://www.youtube.com/watch?v=dQw4w9WgXcQ&pbjreload=101",
+	"../Secret/secret.html",
+];
+var randomNumber = Math.floor(Math.random() * linksArray.length);
+var link = linksArray[randomNumber];
+
+console.log(`Changed link in text to ${link}`);
+document.getElementById("link").setAttribute("href", link);
+
+console.log(`Redirecting to ${link}`);
+setTimeout(() => (window.location.href = link), 1000);
